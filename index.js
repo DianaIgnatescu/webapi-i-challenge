dddc90daa4d720e467f879d074517b158173681b
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-03-18 14:37:25 +0000

    MVP: Complete endpoiint for POST request

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // implement your API here
 const express = require('express');
-
 const db = require('./data/db');
 
 const server = express();
@@ -26,16 +25,38 @@ server.get('/api/users/:id', (req, res) => {
   db.findById(id)
     .then(user => {
       if (!user) {
-        res.status(404).json({ message: "The user with the specified ID does not exist."});
+        res.status(404).json({ message: "The user with the specified ID does not exist." });
       }
       res.status(200).json(user);
     })
     .catch(error => {
       res.status(500).json({ error: "The user information could not be retrieved." });
     })
-})
+});
 
+server.post('/api/users', (req, res) => {
+  const name = req.body.name;
+  const bio = req.body.bio;
+  if (!name || !bio) {
+    res.status(400).json({ errorMessage: "Please provide name and bio for the user."})
+  }
+  const created_at = new Date();
+  const updated_at = created_at;
+  db.insert({ name, bio, created_at, updated_at })
+    .then(data => {
+      res.status(201).json({
+          id: data.id,
+          name,
+          bio,
+          created_at,
+          updated_at,
+      });
+    })
+    .catch(error => {
+      res.status(500).json({ error: "There was an error while saving the user to the database." })
+    })
 
+});
 
 server.listen(5000, () =>
   console.log(`Listening on port 5000`));
